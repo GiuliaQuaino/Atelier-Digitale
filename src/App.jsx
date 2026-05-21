97b232467406c871ea7b894713c0f08bdc9a3959
Zawadi Emilia Piccolo <zawadiemilia.piccolo.24@stud.itsaltoadriatico.it>
2026-05-21 13:26:20 +0200

    Aggiunto RentStatusWidget in sottocartella con CSS moduli

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
-
-import './App.css'
-import Sidebar from './components/sidebar';
-/*import { useState } from "react";
-import { AuthProvider } from "./AuthContext";
-import LoginPage from "./LoginPage";*/
+import React from 'react';
+import RentStatusWidget from './components/RentStatusWidget/RentStatusWidget';
 
 function App() {
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
-import { AuthProvider } from './context/AuthContext';
-import Login from './pages/Login';
-
-export default function App() {
   return (
     <div>
-      <Sidebar></Sidebar>
-      Primi commit
+      <RentStatusWidget />
     </div>
   );
 }
 
-export default App
-
-
-/*
-function App() {
-  const [loggato, setLoggato] = useState(false);
-
-  return (
-    <AuthProvider>
-      {loggato
-        ? <Sidebar />
-        : <LoginPage onLogin={() => setLoggato(true)} />
-      }
-    </AuthProvider>
-  );
-}
-
-export default App
-
-*/
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-
-          <Route path="/login" element={<Login />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
-}
+export default App;
