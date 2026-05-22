811b27ab0ca24941f08cf36feb35f6d323eadc7d
Giulia Quaino <giulia.quaino.24@stud.itsaltoadriatico.it>
2026-05-22 14:25:30 +0200

    Creazione file base componente noleggio - inserimento dati grafico

---
+++ src/components/component-grafico-noleggio/widgetNoleggio.jsx
@@ -0,0 +1,14 @@
+const data = [
+  { month: "Jan", value: 620 },
+  { month: "Feb", value: 950 },
+  { month: "Mar", value: 760 },
+  { month: "Apr", value: 1030 },
+  { month: "May", value: 985 },
+  { month: "Jun", value: 810 },
+  { month: "Jul", value: 820 },
+  { month: "Aug", value: 910 },
+  { month: "Sep", value: 190 },
+  { month: "Oct", value: 920 },
+  { month: "Nov", value: 790 },
+  { month: "Dec", value: 1000 },
+];
